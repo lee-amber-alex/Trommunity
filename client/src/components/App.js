b1b4493373a57f76f99363c4ccfd637570ffa41c
Amber Lee <lee.amber.alex@gmail.com>
2021-03-02 15:11:41 -0800

    Renamed file, deleted container.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
-
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import Dashboard from "./Dashboard/Dashboard";
-// import AuthPage from "../pages/Auth";
 import PrivateRoute from "./PrivateRoute";
 import Profile from "./Dashboard/Profile";
-// import Footer from "../components/Footer/Footer";
 import Home from "../pages/Home";
 import TradePostFormPage from "../pages/TradePostFormPage";
-import Signup from "./signup";
+import Signup from "./Signup";
 import Login from "./Login";
 import "./App.css";
 import TransHistory from "./History/TransHistory";
@@ -19,11 +15,6 @@ function App() {
   return (
     <>
     <Headers/>
-    {/* <Container
-    
-      className="align-items-center"
-      style={{ minHeight: "100vh" }}
-    > */}
       
       <div className="w-100" style={{ maxWidth: "450px" }}>
         <Router>
@@ -43,36 +34,10 @@ function App() {
           </AuthProvider>
         </Router>
       </div>
-    {/* </Container> */}
+    
     </>
   );
 }
 
 export default App;
 
-// const App = () => {
-//   return (
-//     <Router>
-//       <div className="application">
-//         <main className="main">
-//           <Container>
-//             <AuthProvider>
-//               <Switch>
-//                 <PrivateRoute exact path="/" component={Home} />
-//                 <PrivateRoute
-//                   exact
-//                   path="/tradepost"
-//                   component={TradePostFormPage}
-//                 />
-//                 {/* <Route path="/auth" component={AuthPage} /> */}
-//               </Switch>
-//             </AuthProvider>
-//           </Container>
-//           <Footer />
-//         </main>
-//       </div>
-//     </Router>
-//   );
-// };
-
-// export default App;
