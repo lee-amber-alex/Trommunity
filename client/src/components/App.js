087d77fd167ef470d6e26f0a5fa0ff6919e5dd62
Amber Lee <lee.amber.alex@gmail.com>
2021-03-05 12:17:02 -0800

    Adjusted links.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,33 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
-import Profile from "./Dashboard/Profile";
 import Home from "../pages/Home";
-import TradePostFormPage from "../pages/TradePostFormPage";
 import Signup from "./Signup";
 import Login from "./Login";
+import TradeFormPage from "../pages/TradeFormPage";
 import "./App.css";
-import TransHistory from "./History/TransHistory";
-import Headers from "./Header/Header"
+
+
 
 function App() {
   return (
     <>
-    <Headers/>
+   
+    
       
-      <div className="w-100" style={{ maxWidth: "450px" }}>
+      <div >
         <Router>
           <AuthProvider>
             <Switch>
+              
               <PrivateRoute exact path="/" component={Home} />
-              <PrivateRoute exact path="/profile" component={Profile} />
               <PrivateRoute
                 exact
                 path="/tradepost"
-                component={TradePostFormPage}
+                component={TradeFormPage}
               />
-              <PrivateRoute exact path="/transhistory" component={TransHistory} />
               <Route path="/signup" component={Signup} />
               <Route path="/login" component={Login} />
             </Switch>
