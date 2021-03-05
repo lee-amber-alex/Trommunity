6b327cf0ffe1a71db174843282a168ff146de6e7
Amber Lee <lee.amber.alex@gmail.com>
2021-03-05 12:18:23 -0800

    Added profile page.

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProfileCard from "../components/Profile/ProfileCard"
+
+export default function Profile() {
+    return (
+        <>
+            <ProfileCard/>
+            
+        </>
+    )
+}
