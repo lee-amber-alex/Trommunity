592a0ccda3f9ffa21918d7543a661df7c8db8db6
Amber Lee <lee.amber.alex@gmail.com>
2021-03-05 12:18:13 -0800

    Added profile page.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,19 +1,15 @@
 import React from "react";
-// import SideNavbar from "../components/Nav/SideNavbar";
 import Nav from "../components/Nav/Nav"
-// import HomeDashboard from "../components/HomeDashboard/HomeDashboard";
+import Profile from "./Profile";
 
-const styles = {
-  margin: {
-    margin: "20px",
-  },
-};
+
+
 const Home = () => {
   return (
     <div>
       
       <Nav/>
-      
+      <Profile/>
     </div>
   );
 };
