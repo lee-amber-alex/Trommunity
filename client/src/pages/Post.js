0c0316723c90e5f55629b110ed1720ebc31d3d33
Amber Lee <lee.amber.alex@gmail.com>
2021-03-20 00:41:33 -0700

    Created new pages for nav.

---
+++ client/src/pages/Post.js
@@ -0,0 +1,17 @@
+import React from "react";
+import TradeForm from "../components/TradeForm/TradeForm";
+
+
+import "../components/Styles/home.css";
+
+const Post = () => {
+  return (
+    <div>
+      <div className="container">
+        <TradeForm />
+      </div>
+    </div>
+  );
+};
+
+export default Post;
