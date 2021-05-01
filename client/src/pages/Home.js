854091a4a55faceda09ea6b8e4fe63fac47421e9
Amber Lee <lee.amber.alex@gmail.com>
2021-04-30 23:17:38 -0700

    Added dashboard.

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
-import Nav from "../components/Nav/Nav"
-import Profile from "./Profile";
-
-
+// import Nav from "../components/Nav/Nav";
+import Navbar from "../components/Nav/Navbar";
+import ProfileCard from "../components/Profile/ProfileCard";
+import Dashboard from "../components/Dashboard/Dashboard"
 
 const Home = () => {
+
+
   return (
     <div>
+      <Navbar />
+      <ProfileCard />
+      <Dashboard />
       
-      <Nav/>
-      <Profile/>
     </div>
   );
 };
